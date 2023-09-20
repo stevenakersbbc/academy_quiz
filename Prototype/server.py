14309822670751b33480f2cd2f95afbc9b79293f
stevenakersbbc <144691120+stevenakersbbc@users.noreply.github.com>
2023-09-20 10:34:44 +0100

    Server Post testing

--- Prototype/server.py
+++ Prototype/server.py
@@ -11,7 +11,17 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(jsonData.encode())
 
     #Add a post method to allow metrics sending back
-    
+
+    def do_POST(self):
+        post_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+
+        telemetry_post = json.loads(post_body)
+        print(telemetry_post)
+
+
+
 jsonData = json.loads(open('myquizdata.json').read())
+telemetry = json.loads(open('telemetry.json').read())
 httpd = HTTPServer(('localhost', 4000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
