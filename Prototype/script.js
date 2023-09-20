512f103a8def7a89e34871cd950e8be002bffb76
Dean McManus <dean_mcmanus@aol.com>
2023-09-20 12:18:19 +0100

    Added submit functionality, shows result page

--- Prototype/script.js
+++ Prototype/script.js
@@ -1,34 +1,38 @@
+let totalScore = 0;
+let questionCount = 0;
+
 function generateElements(data) {
     data.jillsquiz.forEach((element, index) => {
-    let ques = element.quizquestions[0].question;
-    let topic = element.topic;
-    let ans = element.quizquestions[0].correct_answer;
-    let ans1 = element.quizquestions[0].answers[0];
-    let ans2 = element.quizquestions[0].answers[1];
-    let ans3 = element.quizquestions[0].answers[2];
-    let buttonName = `${topic}-button`
+      questionCount++;
+      let ques = element.quizquestions[0].question;
+      let topic = element.topic;
+      let ans = element.quizquestions[0].correct_answer;
+      let ans1 = element.quizquestions[0].answers[0];
+      let ans2 = element.quizquestions[0].answers[1];
+      let ans3 = element.quizquestions[0].answers[2];
+      let buttonName = `${topic}-button`
 
-    let mytemplate = `<div>
-        <p>Question - ${ques}</p>
-        <p>
-            <label for="${topic}">Choose an answer:</label>
-            <select name="${topic}" id="${topic}">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-            <ol>
-                <li>${ans1}</li>
-                <li>${ans2}</li>
-                <li>${ans3}</li>
-            </ol>
-        </p>
-        <button id="${buttonName}" onclick="check('${topic}', ${ans})">Check this answer</button>
-        <br/><br/>
-      </div>`;
+      let mytemplate = `<div>
+          <p>Question - ${ques}</p>
+          <p>
+              <label for="${topic}">Choose an answer:</label>
+              <select name="${topic}" id="${topic}">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <ol>
+                  <li>${ans1}</li>
+                  <li>${ans2}</li>
+                  <li>${ans3}</li>
+              </ol>
+          </p>
+          <button id="${buttonName}" onclick="check('${topic}', ${ans})">Check this answer</button>
+          <br/><br/>
+        </div>`;
 
-    document.getElementById("mainContent").innerHTML = document.getElementById("mainContent").innerHTML + mytemplate;
-    });
+      document.getElementById("mainContent").innerHTML = document.getElementById("mainContent").innerHTML + mytemplate;
+      });
   } 
 
 var xmlhttp = new XMLHttpRequest();
@@ -49,20 +53,32 @@ function check(thistopic, correctanswer) {
   let answervalue = document.getElementById(thistopic).value;
   if (+answervalue == +correctanswer) {
     fetch("http://localhost:4000", {
-    method: "POST",
-    body: JSON.stringify({
-      userId: 1,
-      title: "Fix my bugs",
-      completed: false
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
+        method: "POST",
+        body: JSON.stringify({
+          userId: 1,
+          title: "Fix my bugs",
+          completed: false
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+    })
     .then((response) => response.json())
     .then((json) => console.log(json));
       alert("Correct");
+      totalScore++;
   } else {
-      alert("Not Correct");
+    alert("Not Correct");
   }
+}
+
+function submitQuestions() {
+  document.getElementById("mainContent").style.display = "none"
+  document.getElementById("submit").style.display = "none"
+
+  //Create text with "You scored: x/y"
+  let percentageResult = totalScore/questionCount * 100
+  let roundedResult = Math.round(percentageResult * 10) / 10
+  let resultsHTML = `<div id=resultsText>You scored: ${roundedResult}%</div>`
+  document.getElementById("resultsArea").innerHTML += resultsHTML
 }
